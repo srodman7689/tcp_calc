1443c5dc387579f3a94f668391c928e6fac2515b
Sean Rodman <srodman7689@gmail.com>
2015-10-15 14:17:29 -0500

    Starting on the Server Code with TDD

--- lib/tcpcalc.rb
+++ lib/tcpcalc.rb
@@ -1,4 +1,15 @@
 require "tcpcalc/version"
 
-module TCPCalc
+class TCPCalc
+  attr_reader :port
+  
+  DEFAULT_PORT = 6789
+
+  def initialize
+  end
+
+  def start(port=DEFAULT_PORT)
+    @port = port
+    true
+  end
 end
--- spec/tcpcalc_spec.rb
+++ spec/tcpcalc_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
 describe TCPCalc do
+  before(:each) do
+    @tcpcalc = TCPCalc.new
+  end
+
   it 'has a version number' do
     expect(TCPCalc::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(true).to eq(true)
+  it 'starts when told to start' do
+    started = @tcpcalc.start()
+    expect(started).to eq(true)
+  end
+
+  it 'starts on default port when not given one' do
+    @tcpcalc.start()
+    port_num = @tcpcalc.port
+    expect(port_num).to eq(TCPCalc::DEFAULT_PORT)
+  end
+
+  it 'starts on a specific port when given one' do
+    expected_port = 4567
+    @tcpcalc.start(expected_port)
+    port_num = @tcpcalc.port
+    expect(port_num).to eq(expected_port)
   end
 end
