70dcb7bcf0d90af9cecdfe85af218e4fa1548b88
Sean Rodman <srodman7689@gmail.com>
2015-10-15 13:56:40 -0500

    added configuration for rspec to the spec_helper.rb file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'tcpcalc'
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+  
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
--- spec/tcpcalc_spec.rb
+++ spec/tcpcalc_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Tcpcalc do
+describe TCPCalc do
   it 'has a version number' do
-    expect(Tcpcalc::VERSION).not_to be nil
+    expect(TCPCalc::VERSION).not_to be nil
   end
 
   it 'does something useful' do
